ad95c8c8ee5ee84c11e8de53bcb55e895572eaaf
varad <varadsoham555@gmail.com>
2022-04-15 11:25:51 +0530

    add post DTO,  return DTO in posts, remove seedpost

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,4 +1,5 @@
 using Blog.Data;
+using Blog.Models.DTOs;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,9 +24,13 @@ app.UseHttpsRedirection();
 // TODO: Refactor to Services and Repositories
 // TODO: Write Unit Tests
 
-app.MapGet("/posts", (BlogDbContext context, HttpContext httpContext) =>
+app.MapGet("/posts",async (BlogDbContext context, HttpContext httpContext) =>
 {
-    return context.Posts.Include(x => x.Author).ToListAsync();
+    var postDTO = context.Posts.Select(p=> new PostDTO() 
+    { 
+        Id=p.Id, Title = p.Title, Body = p.Body, PublishedAt = p.PublishedAt, AuthorId = p.AuthorId
+    }); 
+    return postDTO;
 });
 
 app.MapGet("/posts/{postId}", (BlogDbContext context, int postId) =>
@@ -50,12 +55,6 @@ app.MapDelete("/delblog/{id}", async (BlogDbContext context, int id) =>
     }      
     return Results.Ok();
 });
-app.MapPost("/seedpost", async (BlogDbContext context) =>
- {
-     await context.Posts.AddAsync(new Blog.Models.Post { Author = new Blog.Models.Author { Name = "Varad", ImageUrl = "sample.png" }, Body = "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione.Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione.", PublishedAt = DateTime.UtcNow, Tags = new string[] { "hello", "world" }, Title = "Sqlite" });
-     await context.SaveChangesAsync();
-     return Results.Ok();
- });
 
 using (var scope = app.Services.CreateScope())
 using (var context = scope.ServiceProvider.GetRequiredService<BlogDbContext>())
